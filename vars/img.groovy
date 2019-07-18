ef06122615cb7da93f313277ea33c6de97f9c4ce
yuanfang <yuanfang@alauda.io>
2019-07-18 19:06:21 +0800

    wip

--- vars/img.groovy
+++ vars/img.groovy
@@ -1,7 +1,7 @@
 def genPushSh(Map params){
-
-    return 
-
+    def engine = new groovy.text.SimpleTemplateEngine()
+    def template = engine.createTemplate(params.push).make(params.params)
+    return template.toString()
 }
 
 def genBuildSh(Map params) {
@@ -11,3 +11,26 @@ def genBuildSh(Map params) {
     def template = engine.createTemplate(params.script).make(params.params)
     return template.toString()
 }
+
+def replace(String str, from,to){
+    def _from = printf("\${%s}",from)
+    return str.replaceAll(_from,to)
+}
+
+def test(){
+    def tt = 'docker pull ${OriginalImage} && docker tag ${OriginalImage} ${REGISTRY}/${OWNER}/${ImageName}'
+    def pp= ['REGISTRY':'index.alauda.cn','OWNER':'alaudaorg','ImageName':'base-notebook','OriginalImage':'jupyter/base-notebook']
+
+    pp.each{ k,v->
+        println(k)
+        tt = replace(tt,k,v)
+    }
+    println tt
+
+}
+
+// test()
+// def tt = 'docker pull ${OriginalImage} && docker tag ${OriginalImage} ${REGISTRY}/${OWNER}/${ImageName}'
+// tt.replaceAll('{REGISTRY}',"aaa")
+
+println genBuildSh([:])
