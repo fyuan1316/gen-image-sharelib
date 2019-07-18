c6ac2d3603ba035547be86ba5ebfcfbee015de6b
yuanfang <yuanfang@alauda.io>
2019-07-18 16:36:03 +0800

    init

---
+++ vars/img.groovy
@@ -0,0 +1,17 @@
+def genBuildSh(Map params){
+
+
+    return 
+
+}
+
+def genPushSh(Map params){
+
+    return 
+
+}
+
+def genSh(Map params) String {
+    params.script += 'haha'
+    return params.script
+}
