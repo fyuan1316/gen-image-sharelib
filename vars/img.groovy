ff7ea85601f2da10e24612a5c711614fc1d7de38
yuanfang <yuanfang@alauda.io>
2019-07-18 17:50:52 +0800

    wip

--- vars/img.groovy
+++ vars/img.groovy
@@ -1,17 +1,13 @@
-def genBuildSh(Map params){
-
-
-    return 
-
-}
-
 def genPushSh(Map params){
 
     return 
 
 }
 
-def genSh(Map params) {
-    params.script += 'haha'
-    return params.script
+def genBuildSh(Map params) {
+    // params.script = 'docker pull ${OriginalImage} && docker tag ${OriginalImage} ${REGISTRY}/${OWNER}/${ImageName}'
+    // params.params = ['REGISTRY':'index.alauda.cn','OWNER':'alaudaorg','ImageName':'base-notebook','OriginalImage':'jupyter/base-notebook']
+    def engine = new groovy.text.SimpleTemplateEngine()
+    def template = engine.createTemplate(params.script).make(params.params)
+    return template.toString()
 }
