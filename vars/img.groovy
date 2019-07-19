21475a8795aa3fcbb011340951622c716a788ed7
yuanfang <yuanfang@alauda.io>
2019-07-19 13:12:06 +0800

    wip

--- vars/img.groovy
+++ vars/img.groovy
@@ -1,36 +1,22 @@
 def genPushSh(Map params){
-    def engine = new groovy.text.SimpleTemplateEngine()
-    def template = engine.createTemplate(params.push).make(params.params)
-    return template.toString()
+    return replace(params.params, params.push)
 }
 
 def genBuildSh(Map params) {
     // params.script = 'docker pull ${OriginalImage} && docker tag ${OriginalImage} ${REGISTRY}/${OWNER}/${ImageName}'
     // params.params = ['REGISTRY':'index.alauda.cn','OWNER':'alaudaorg','ImageName':'base-notebook','OriginalImage':'jupyter/base-notebook']
-    def engine = new groovy.text.SimpleTemplateEngine()
-    def template = engine.createTemplate(params.script).make(params.params)
-    return template.toString()
+    return replace(params.params, params.script)
 }
 
-def replace(String str, from,to){
-    def _from = printf("\${%s}",from)
-    return str.replaceAll(_from,to)
-}
-
-def test(){
-    def tt = 'docker pull ${OriginalImage} && docker tag ${OriginalImage} ${REGISTRY}/${OWNER}/${ImageName}'
-    def pp= ['REGISTRY':'index.alauda.cn','OWNER':'alaudaorg','ImageName':'base-notebook','OriginalImage':'jupyter/base-notebook']
-
-    pp.each{ k,v->
-        println(k)
-        tt = replace(tt,k,v)
+def replace(Map map, String string) {
+    for (def data : map) {
+        string = string.replace("\${" + data.getKey() + "}", data.getValue().toString())
     }
-    println tt
-
+    return string
 }
 
-// test()
+// println genBuildSh([:])
 // def tt = 'docker pull ${OriginalImage} && docker tag ${OriginalImage} ${REGISTRY}/${OWNER}/${ImageName}'
-// tt.replaceAll('{REGISTRY}',"aaa")
-
-println genBuildSh([:])
+// def pp= ['REGISTRY':'index.alauda.cn','OWNER':'alaudaorg','ImageName':'base-notebook','OriginalImage':'jupyter/base-notebook']
+// def out = replace(pp,tt)
+// println out
